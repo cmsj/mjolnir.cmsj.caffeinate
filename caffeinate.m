d52bd99b876db1b704224f41737b4c11f4d25541
Chris Jones <cmsj@tenshu.net>
2014-09-04 21:05:54 +0100

    blah

--- caffeinate.m
+++ caffeinate.m
@@ -3,6 +3,8 @@
 #import <IOKit/pwr_mgt/IOPMLib.h>
 #import <lauxlib.h>
 
+static IOPMAssertionID NoDisplaySleep;
+
 static int caffeinate_addnumbers(lua_State* L) {
     int a = luaL_checknumber(L, 1);
     int b = luaL_checknumber(L, 2);
@@ -21,6 +23,8 @@ static const luaL_Reg caffeinatelib[] = {
          must match the require-path of this file, i.e. "mjolnir.cmsj.caffeinate.internal". */
 
 int luaopen_mjolnir_cmsj_caffeinate_internal(lua_State* L) {
+    noDisplaySleep = nil;
+
     luaL_newlib(L, caffeinatelib);
     return 1;
 }
