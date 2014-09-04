2651aad8b4e2a6e550e349d893b9e9c6bc1bec7e
Chris Jones <cmsj@tenshu.net>
2014-09-04 21:55:59 +0100

    Add some debugging

--- caffeinate.m
+++ caffeinate.m
@@ -19,14 +19,35 @@ static int caffeinate_prevent_display_sleep(lua_State* L) {
                                                 NULL,
                                                 &noDisplaySleep);
     if (result != kIOReturnSuccess) {
+        printf("prevent_display_sleep: assertion failed");
         // WAT DO
     }
+
+    printf("prevent_display_sleep: noDisplaySleep = %u", noDisplaySleep);
+
+    return 1;
+}
+
+static int caffeinate_allow_display_sleep(lua_State* L) {
+    IOReturn result = 1;
+
+    if (!noDisplaySleep) return 1;
+
+    result = IOPMAssertionRelease(noDisplaySleep);
+    if (result != kIOReturnSuccess) {
+        printf("allow_display_sleep: release failed");
+        // WAT DO
+    }
+
+    noDisplaySleep = 0;
+    printf("allow_display_sleep: noDisplaySleep = %u", noDisplaySleep);
 
     return 1;
 }
 
 static const luaL_Reg caffeinatelib[] = {
     {"prevent_display_sleep", caffeinate_prevent_display_sleep},
+    {"allow_display_sleep", caffeinate_allow_display_sleep},
     
     {} // necessary sentinel
 };
@@ -37,5 +58,6 @@ static const luaL_Reg caffeinatelib[] = {
 
 int luaopen_mjolnir_cmsj_caffeinate_internal(lua_State* L) {
     luaL_newlib(L, caffeinatelib);
+    printf("Initialised caffeinate. noDisplaySleep = %u", noDisplaySleep);
     return 1;
 }
