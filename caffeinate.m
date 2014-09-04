bc5540ecc071b10111be0e7da59e43c57585c72d
Chris Jones <cmsj@tenshu.net>
2014-09-04 20:58:22 +0100

    slowly creep towards what we want

--- caffeinate.m
+++ caffeinate.m
@@ -1,25 +1,26 @@
 #import <Cocoa/Cocoa.h>
 #import <Carbon/Carbon.h>
+#import <IOKit/pwr_mgt/IOPMLib.h>
 #import <lauxlib.h>
 
-static int foobar_addnumbers(lua_State* L) {
+static int caffeinate_addnumbers(lua_State* L) {
     int a = luaL_checknumber(L, 1);
     int b = luaL_checknumber(L, 2);
     lua_pushnumber(L, a + b);
     return 1;
 }
 
-static const luaL_Reg foobarlib[] = {
-    {"addnumbers", foobar_addnumbers},
+static const luaL_Reg caffeinatelib[] = {
+    {"addnumbers", caffeinate_addnumbers},
     
     {} // necessary sentinel
 };
 
 
-/* NOTE: The substring "mjolnir_yourid_foobar_internal" in the following function's name
-         must match the require-path of this file, i.e. "mjolnir.yourid.foobar.internal". */
+/* NOTE: The substring "mjolnir_cmsj_caffeinate_internal" in the following function's name
+         must match the require-path of this file, i.e. "mjolnir.cmsj.caffeinate.internal". */
 
-int luaopen_mjolnir_yourid_foobar_internal(lua_State* L) {
-    luaL_newlib(L, foobarlib);
+int luaopen_mjolnir_cmsj_caffeinate_internal(lua_State* L) {
+    luaL_newlib(L, caffeinatelib);
     return 1;
 }
