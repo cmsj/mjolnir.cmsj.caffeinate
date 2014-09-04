e499d42917c1fded786eaa0025cf4ca39e96e8ab
Chris Jones <cmsj@tenshu.net>
2014-09-04 20:16:13 +0100

    add upload script

---
+++ upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AUTHOR="cmsj"
+MODULE="caffeinate"
+
+if [ "$1" == "" ]; then
+    echo "Usage: upload.sh VERSION"
+    exit 1
+fi
+
+VERSION="$1"
+
+luarocks pack mjolnir.${AUTHOR}.${MODULE}
+moonrocks upload --skip-pack mjolnir.${AUTHOR}.${MODULE}-${VERSION}.rockspec
+moonrocks upload mjolnir.${AUTHOR}.${MODULE}-${VERSION}.all.rock
