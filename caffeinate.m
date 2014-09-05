cc126fcfcebd0efc14cb285ccb2519ae537e6fb7
Chris Jones <cmsj@tenshu.net>
2014-09-05 10:14:55 +0100

    Refactor and add two additional power assertion types

--- caffeinate.m
+++ caffeinate.m
@@ -3,63 +3,132 @@
 #import <IOKit/pwr_mgt/IOPMLib.h>
 #import <lauxlib.h>
 
-static IOPMAssertionID noDisplaySleep = 0;
+static IOPMAssertionID noIdleDisplaySleep = 0;
+static IOPMAssertionID noIdleSystemSleep = 0;
+static IOPMAssertionID noSystemSleep = 0;
 
+// Debug/error output helper, prints to Mjolnir's console
 static void caffeinate_print(lua_State *L, char *msg) {
     lua_getglobal(L, "print");
     lua_pushstring(L, msg);
     lua_call(L, 1, 0);
 }
 
-static int caffeinate_prevent_display_sleep(lua_State *L) {
+// Create an IOPM Assertion of specified type and store its ID in the specified variable
+static void caffeinate_create_assertion(lua_State *L, CFStringRef assertionType, IOPMAssertionID assertionID) {
     IOReturn result = 1;
 
-    if (noDisplaySleep) return 0;
+    if (assertionID) return;
 
-    result = IOPMAssertionCreateWithDescription(kIOPMAssertionTypePreventUserIdleDisplaySleep,
+    result = IOPMAssertionCreateWithDescription(assertionType,
                                                 CFSTR("mjolnir.cmsj.caffeinate"),
                                                 NULL,
                                                 NULL,
                                                 NULL,
                                                 0,
                                                 NULL,
-                                                &noDisplaySleep);
+                                                &assertionID);
+
     if (result != kIOReturnSuccess) {
-        caffeinate_print(L, "prevent_display_sleep: assertion failed");
-        // WAT DO
+        caffeinate_print(L, "caffeinate_create_assertion: failed");
     }
-
-    caffeinate_print(L, "prevent_display_sleep: success");
-
-    return 0;
 }
 
-static int caffeinate_allow_display_sleep(lua_State *L) {
+// Release a previously stored assertion
+static void caffeinate_release_assertion(lua_State *L, IOPMAssertionID assertionID) {
     IOReturn result = 1;
 
-    if (!noDisplaySleep) return 0;
+    if (!assertionID) return;
 
-    result = IOPMAssertionRelease(noDisplaySleep);
+    result = IOPMAssertionRelease(assertionID);
+
     if (result != kIOReturnSuccess) {
-        caffeinate_print(L, "allow_display_sleep: release failed");
-        // WAT DO
+        caffeinate_print(L, "caffeinate_release_assertion: failed");
     }
+}
 
-    noDisplaySleep = 0;
-    caffeinate_print(L, "allow_display_sleep: success");
+// ----------------------- Functions for display sleep when user is idle ----------------------------
 
+// Prevent display sleep if the user goes idle (and by implication, system sleep)
+static int caffeinate_prevent_idle_display_sleep(lua_State *L) {
+    caffeinate_create_assertion(L, kIOPMAssertionTypePreventUserIdleDisplaySleep, noIdleDisplaySleep);
     return 0;
 }
 
+// Allow display sleep if the user goes idle
+static int caffeinate_allow_idle_display_sleep(lua_State *L) {
+    caffeinate_release_assertion(L, noIdleDisplaySleep);
+    return 0;
+}
+
+// Determine if idle display sleep is currently prevented
+static int caffeinate_is_idle_display_sleep_prevented(lua_State *L) {
+    lua_pushboolean(L, (noIdleDisplaySleep != 0));
+    return 1;
+}
+
+// ----------------------- Functions for system sleep when user is idle ----------------------------
+
+// Prevent system sleep if the user goes idle (display may still sleep)
+static int caffeinate_prevent_idle_system_sleep(lua_State *L) {
+    caffeinate_create_assertion(L, kIOPMAssertionTypePreventUserIdleSystemSleep, noIdleSystemSleep);
+    return 0;
+}
+
+// Allow system sleep if the user goes idle
+static int caffeinate_allow_idle_system_sleep(lua_State *L) {
+    caffeinate_release_assertion(L, noIdleSystemSleep);
+    return 0;
+}
+
+// Determine if idle system sleep is currently prevented
+static int caffeinate_is_idle_system_sleep_prevented(lua_State *L) {
+    lua_pushboolean(L, (noIdleSystemSleep != 0));
+    return 1;
+}
+
+// ----------------------- Functions for system sleep ----------------------------
+
+// Prevent system sleep (only when on AC power)
+static int caffeinate_prevent_system_sleep(lua_State *L) {
+    caffeinate_create_assertion(L, kIOPMAssertionTypePreventSystemSleep, noSystemSleep);
+    return 0;
+}
+
+// Allow system sleep
+static int caffeinate_allow_system_sleep(lua_State *L) {
+    caffeinate_release_assertion(L, noSystemSleep);
+    return 0;
+}
+
+// Determine if system sleep is currently prevented
+static int caffeinate_is_system_sleep_prevented(lua_State *L) {
+    lua_pushboolean(L, (noSystemSleep != 0));
+    return 1;
+}
+
+// ----------------------- Lua/Mjolnir glue GAR ---------------------
+
 static int caffeinate_gc(lua_State *L) {
-    if (noDisplaySleep) caffeinate_allow_display_sleep(L);
+    // TODO: We should register which of the assertions we have active, somewhere that persists a reload()
+    if (noIdleDisplaySleep) caffeinate_allow_idle_display_sleep(L);
 
     return 0;
 }
 
 static const luaL_Reg caffeinatelib[] = {
-    {"prevent_display_sleep", caffeinate_prevent_display_sleep},
-    {"allow_display_sleep", caffeinate_allow_display_sleep},
+    {"prevent_idle_display_sleep", caffeinate_prevent_idle_display_sleep},
+    {"allow_idle_display_sleep", caffeinate_allow_idle_display_sleep},
+    {"is_idle_display_sleep_prevented", caffeinate_is_idle_display_sleep_prevented},
+
+    {"prevent_idle_system_sleep", caffeinate_prevent_idle_system_sleep},
+    {"allow_idle_system_sleep", caffeinate_allow_idle_system_sleep},
+    {"is_idle_system_sleep_prevented", caffeinate_is_idle_display_sleep_prevented},
+
+    {"prevent_system_sleep", caffeinate_prevent_system_sleep},
+    {"allow_system_sleep", caffeinate_allow_system_sleep},
+    {"is_system_sleep_prevented", caffeinate_is_system_sleep_prevented},
+
     {"__gc", caffeinate_gc},
     
     {} // necessary sentinel
@@ -71,6 +140,5 @@ static const luaL_Reg caffeinatelib[] = {
 
 int luaopen_mjolnir_cmsj_caffeinate_internal(lua_State *L) {
     luaL_newlib(L, caffeinatelib);
-    caffeinate_print(L, "Initialised caffeinate.");
     return 1;
 }
